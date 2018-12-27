4432a037a1d67f66fe1f87eae4ff29025fca911d
Oliver Gomes <ogomesphoto@gmail.com>
2018-12-26 19:08:47 -0500

    duplicate more components

--- components/src/index.js
+++ components/src/index.js
@@ -6,6 +6,37 @@ import faker from "faker";
 const App = () => {
   return (
     <div className="ui container comments">
+      {/* 1 */}
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src={faker.image.avatar()} alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+      {/* 2 */}
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src={faker.image.avatar()} alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+      {/* 3 */}
       <div className="comment">
         <a href="/" className="avatar">
           <img src={faker.image.avatar()} alt="avatar" />
