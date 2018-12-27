940a63a6b5387da1b76e79ce6046a18fbcd71382
Oliver Gomes <ogomesphoto@gmail.com>
2018-12-27 00:57:10 -0500

    Approval component wrap around CommentDetail

--- components/src/index.js
+++ components/src/index.js
@@ -2,14 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail name="Will" post="I just joined" />
-      <CommentDetail name="Sam" post="I just joined too" />
-      <CommentDetail name="Amanda" post="Glad to be here" />
-      <CommentDetail name="Raj" post="Wow!" />
+      <ApprovalCard>
+        <CommentDetail
+          name="Will"
+          post="I just joined"
+          timeAgo="Today at 4:45PM"
+        />
+      </ApprovalCard>
+      <CommentDetail
+        name="Sam"
+        post="I just joined too"
+        timeAgo="Today at 2:00PM"
+      />
+      <ApprovalCard>
+        <CommentDetail
+          name="Amanda"
+          post="Glad to be here"
+          timeAgo="Today at 7:15PM"
+        />
+      </ApprovalCard>
+      <CommentDetail name="Raj" post="Wow!" timeAgo="Yesterday at 12:05PM" />
     </div>
   );
 };
