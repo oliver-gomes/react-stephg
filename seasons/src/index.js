e103bbe84efe1a4bc7d70ddef182574bf8133061
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-06 16:42:07 -0500

    class based component implement

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from "react-dom";
 
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  return <div>Hi there</div>;
-};
+class App extends React.Component {
+  state = {
+    latitude: null
+  };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => () => {
+        this.setState({
+          latitude: position.coords.latitude
+        });
+        console.log(this.state.latitude);
+      },
+      err => console.log(err)
+    );
+  }
+  render() {
+    return <div>Latitude: {this.state.latidude}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
