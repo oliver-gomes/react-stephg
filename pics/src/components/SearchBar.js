ad9a1e491a8581c39f99259ceed650eb2e21a62b
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-10 15:03:02 -0500

    onFormSubmit setup

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -5,10 +5,16 @@ class SearchBar extends React.Component {
     term: ""
   };
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(this.state.term);
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
