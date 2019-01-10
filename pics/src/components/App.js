b4b106c6f0c38dfd12754b9617b9a4183b104f62
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-10 18:15:37 -0500

    clean up code

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import unsplash from "../api/unsplash.js";
 import SearchBar from "./SearchBar";
 
-//API = 50ce2fdc20d5d8824690275faa8a56ba37c56b2247aba8ad6dcd2c0f021cb90c
-
 class App extends React.Component {
   state = {
     images: []
