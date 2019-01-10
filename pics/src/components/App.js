904353d32373a2d8e5d986fabf2763f31ba4acc7
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-10 17:37:22 -0500

    setState the images

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,7 +5,11 @@ import SearchBar from "./SearchBar";
 //API = 50ce2fdc20d5d8824690275faa8a56ba37c56b2247aba8ad6dcd2c0f021cb90c
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = {
+    images: []
+  };
+
+  onSearchSubmit = async term => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -13,13 +17,17 @@ class App extends React.Component {
           "Client-ID 50ce2fdc20d5d8824690275faa8a56ba37c56b2247aba8ad6dcd2c0f021cb90c"
       }
     });
-    console.log(response.data.results);
-  }
+
+    this.setState({
+      images: response.data.results
+    });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found {this.state.images.length} images
       </div>
     );
   }
