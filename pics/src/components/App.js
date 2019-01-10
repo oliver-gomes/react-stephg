0a261fca71ff2066c574c47749bf5b60396382d0
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-10 17:08:51 -0500

    axios implement to get pictures

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,20 @@
 import React from "react";
-
+import axios from "axios";
 import SearchBar from "./SearchBar";
+
+//API = 50ce2fdc20d5d8824690275faa8a56ba37c56b2247aba8ad6dcd2c0f021cb90c
+
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      header: {
+        Authorization:
+          "Client-ID 50ce2fdc20d5d8824690275faa8a56ba37c56b2247aba8ad6dcd2c0f021cb90c"
+      }
+    });
   }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
