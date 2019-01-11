316110aa74410e0f93a60fee48d4d04628bd0b43
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-11 14:38:08 -0500

    axios to handle API

--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyD1bKTH5zOCvugiS9xt_4XR0R35yBC0odE";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3/",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
