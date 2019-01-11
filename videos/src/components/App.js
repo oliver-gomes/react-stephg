0770501c509cf37f6b340d0f2f27aa6f924dad63
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-11 18:34:56 -0500

    nested callback for onClick video

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -5,7 +5,8 @@ import VideoList from "./VideoList";
 
 class App extends React.Component {
   state = {
-    videos: []
+    videos: [],
+    selectedVideo: ""
   };
   onTermSubmit = async term => {
     const response = await youtube.get("./search", {
@@ -19,11 +20,18 @@ class App extends React.Component {
     });
   };
 
+  onVideoSelect = video => {
+    console.log("video from the App!", video);
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
