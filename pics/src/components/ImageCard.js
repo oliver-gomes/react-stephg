97dd86ed07c600f5134b06e67d41869781bcbf32
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-10 22:38:20 -0500

    fix span css grid

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,12 +1,34 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      spans: 0
+    };
+
+    this.imageRef = React.createRef();
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img
+          ref={this.imageRef}
           alt={this.props.image.description}
           src={this.props.image.urls.regular}
         />
