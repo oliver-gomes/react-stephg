40d93f02c3fe9a50452218b4ecbf3846711dbeff
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-11 14:48:32 -0500

    onFormSubmit update

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -11,8 +11,7 @@ class SearchBar extends React.Component {
 
   onFormSubmit = e => {
     e.preventDefault();
-
-    //TODO: call callback from parent component
+    this.props.onFormSubmit(this.state.term);
   };
 
   render() {
