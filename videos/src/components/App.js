207809bb5462ba0c9ab817b91e7d2a6ac586e663
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-12 00:53:44 -0500

    First selectedVideo update

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -7,8 +7,12 @@ import VideoDetail from "./VideoDetail";
 class App extends React.Component {
   state = {
     videos: [],
-    selectedVideo: ""
+    selectedVideo: null
   };
+
+  componentDidMount() {
+    this.onTermSubmit("teens react");
+  }
   onTermSubmit = async term => {
     const response = await youtube.get("./search", {
       params: {
@@ -17,7 +21,8 @@ class App extends React.Component {
     });
 
     this.setState({
-      videos: response.data.items
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
     });
   };
 
@@ -29,11 +34,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
