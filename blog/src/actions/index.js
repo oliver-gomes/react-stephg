841dd47e5f8526d38bcbc45093fae6f3d3327afd
Oliver Gomes <ogomesphoto@gmail.com>
2019-02-09 23:11:41 -0500

    Initial commit

--- blog/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-import jsonPlaceholder from "../apis/jsonPlaceholder";
-
-export const fetchPosts = () => {
-  const promise = jsonPlaceholder.get("/posts");
-  return {
-    type: "FETCH_POSTS",
-    payload: promise
-  };
-};
