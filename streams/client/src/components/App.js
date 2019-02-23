28396b8722c6b9211cfc461ba3abc9dcbe34db56
Oliver Gomes <ogomesphoto@gmail.com>
2019-02-22 23:20:10 -0500

    link add

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import "./App.css";
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo">Navigate to Page Two</Link>
+    </div>
+  );
+};
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo <button>Click me</button>
+      <Link to="/">Navigate to Page One</Link>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
-    return <div>APP</div>;
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={PageOne} />
+            <Route path="/pagetwo" component={PageTwo} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
