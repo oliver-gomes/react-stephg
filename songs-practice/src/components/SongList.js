2869001b8b5513c41b4b506214cef6fa2ce2d0c3
Oliver Gomes <ogomesphoto@gmail.com>
2019-04-27 23:30:16 -0400

    dispatch actions

--- songs-practice/src/components/SongList.js
+++ songs-practice/src/components/SongList.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
   renderList() {
     return this.props.songs.map((song, id) => (
       <div className="item" key={id}>
         <div className="right floated content">
-          <button className="ui button primary">select</button>
+          <button
+            className="ui button primary"
+            onClick={() => this.props.selectSong(song)}
+          >
+            select
+          </button>
         </div>
         <div className="content">{song.title}</div>
       </div>
@@ -18,9 +24,16 @@ class SongList extends Component {
 }
 
 const mapStateToProps = state => {
+  console.log(state);
   return {
-    songs: state.songs
+    songs: state.songs,
+    selectedSong: state.selectedSong
   };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(
+  mapStateToProps,
+  {
+    selectSong: selectSong
+  }
+)(SongList);
